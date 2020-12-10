3a85920832bca7ffb92d0ee0e8fd9ab5d9db9de1
nguyenvanduyet <73922657+nguyenvanduyet@users.noreply.github.com>
2020-12-10 09:11:07 +0700

    update

---
+++ src/index.java
@@ -0,0 +1,15 @@
+public class index {
+    public static void main(String[] args) {
+        int i=20;
+        float f=20.5f;
+        double d = 20.5;
+        boolean b=true;
+        char c='a';
+        String s="Hà Nội";
+        System.out.println("i="+i);
+        System.out.println("f="+f);
+        System.out.println("d="+d);
+        System.out.println("b="+b);
+        System.out.println("c="+c);
+    }
+}
